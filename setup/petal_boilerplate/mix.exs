8648314ff9bb034400eee3b0e1cbf2e96c98d975
Matthew Platts <plattsmw@gmail.com>
2022-07-26 20:37:55 +1000

    Remove :gettext from compilers. Update submodules

--- setup/petal_boilerplate/mix.exs
+++ setup/petal_boilerplate/mix.exs
@@ -5,9 +5,9 @@ defmodule PetalBoilerplate.MixProject do
     [
       app: :petal_boilerplate,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
