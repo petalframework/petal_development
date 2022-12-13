96aefcbefe8066751f9fc0ae2a1a2f7419846f39
Matthew Platts <plattsmw@gmail.com>
2022-12-13 16:49:23 +1100

    Update formatter to live view

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,7 @@
 # Used by "mix format"
 [
-  inputs: ["mix.exs", "config/*.exs"],
-  subdirectories: ["apps/*"]
+  import_deps: [:ecto, :phoenix],
+  inputs: ["*.{heex,ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
+  subdirectories: ["priv/*/migrations"],
+  plugins: [Phoenix.LiveView.HTMLFormatter]
 ]
